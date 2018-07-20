097a33965af386926c3dfa5d1443d7d32b8f87f1
Mike McKay <mfmckay@wustl.edu>
2018-07-20 11:58:46 -0500

    Modified spreadsheet importer java backing class to pass receiver ports as well. This will make the page no longer work until it is also converted to use the new REST calls.

--- src/main/java/org/apache/turbine/modules/screens/SpreadsheetBasedImporter.java
+++ src/main/java/org/apache/turbine/modules/screens/SpreadsheetBasedImporter.java
@@ -52,52 +52,40 @@ public class SpreadsheetBasedImporter extends SecureScreen {
             }
 
         };
-        Collections.sort(projects,projRunningTitleComparator);
+        Collections.sort(projects, projRunningTitleComparator);
         context.put("projects", projects);
 
-        ArrayList<ApplicationEntity> aes = new ArrayList<>();
-//        ArrayList<ApplicationEntity> xnatAes = new ArrayList<>();
-//        ArrayList<ApplicationEntity> pacsAes = new ArrayList<>();
-        ArrayList<String> addedAeTitles = new ArrayList<>();
-//        String defaultAe = "";
-        //Get all enabled and storable PACS
-//        final List<Pacs> allPacs = XDAT.getContextService().getBean(PacsEntityService.class).findAllStorable();
-//        for (Pacs pacs : allPacs){
-//            //Only add receivers that are set to storable to list of XNAT AEs
-//            String aeTitle = pacs.getAeTitle();
-//            String label = pacs.getLabel();
-//            if (!StringUtils.isBlank(aeTitle)) {
-//                if (!addedAeTitles.contains(aeTitle)) {
-//                    ApplicationEntity ae = new ApplicationEntity();
-//                    ae.setAeTitle(aeTitle);
-//                    if(!StringUtils.isBlank(label)){
-//                        ae.setLabel(label);
-//                    }
-//                    if (pacs.isDefaultStoragePacs()) {
-//                        ae.setIsDefaultStorageDestination(true);
-//                    }
+        ArrayList<String> aesAndPorts = new ArrayList<>();
+
+        Collection<DicomSCPInstance> scps = XDAT.getContextService().getBean(DicomSCPManager.class).getDicomSCPInstances().values();
+        for (DicomSCPInstance scp : scps){
+            try {
+                String aeTitle = scp.getAeTitle();
+                int port = scp.getPort();
+                String aeAndPort = aeTitle + ":" + port;
+                aesAndPorts.add(aeAndPort);
+            }
+            catch(Exception e){
+                log.error("Exception getting information for one of the SCP receivers",e);
+            }
+        }
+//        ArrayList<ApplicationEntity> aes = new ArrayList<>();
+//        ArrayList<String> addedAeTitles = new ArrayList<>();
+//
+//        Collection<DicomSCPInstance> scps = XDAT.getContextService().getBean(DicomSCPManager.class).getDicomSCPInstances().values();
+//        for (DicomSCPInstance scp : scps){
+//            String aeTitle = scp.getAeTitle();
+//
+//            if(!StringUtils.isBlank(aeTitle)){
+//                ApplicationEntity ae = new ApplicationEntity();
+//                ae.setAeTitle(aeTitle);
+//                if(!addedAeTitles.contains(aeTitle)){
 //                    aes.add(ae);
-//                    pacsAes.add(ae);
 //                }
 //            }
 //        }
 
-        Collection<DicomSCPInstance> scps = XDAT.getContextService().getBean(DicomSCPManager.class).getDicomSCPInstances().values();
-        for (DicomSCPInstance scp : scps){
-            String aeTitle = scp.getAeTitle();
-            if(!StringUtils.isBlank(aeTitle)){
-                ApplicationEntity ae = new ApplicationEntity();
-                ae.setAeTitle(aeTitle);
-//                if(!xnatAes.contains(aeTitle)) {
-//                    xnatAes.add(ae);
-//                }
-                if(!addedAeTitles.contains(aeTitle)){
-                    aes.add(ae);
-                }
-            }
-        }
-
-        Collections.sort(aes);
-        context.put("aes", aes);
+        Collections.sort(aesAndPorts);
+        context.put("aes", aesAndPorts);
     }
 }
