fb5298208193fdf463b884c03d55fc35e552f3f1
Mark M. Florida <markflorida@wustl.edu>
2019-01-14 17:44:02 -0600

    XNATX-719 XNATX-768 "Import from PACS" and "Send to PACS" action links will only show if all necessary DQR criteria are met. The PACS search/import page will show an error message if project is not DQR-enabled, and redirect if user cannot access the project.

--- src/main/resources/META-INF/resources/page/dqr/import/view.js
+++ src/main/resources/META-INF/resources/page/dqr/import/view.js
@@ -2,31 +2,36 @@ $(function(){
 
     var pageBody = $('#page-body');
 
-    function showConfigErrorMessage(){
+    function showConfigErrorMessage(msg){
         $('.datepickers-container').remove();
-        pageBody.removeClass('hidden').find('> .pad').empty().html('' +
-            '<div class="error">' +
-            'This project is not configured for PACS DICOM import. ' +
-            'Please contact your system administrator if you need DICOM PACS import enabled.' +
-            '</div>' +
-            '');
+        pageBody.removeClass('hidden').find('> .pad').empty().html(msg);
     }
 
     window.projectId = window.projectId || getQueryStringValue('project');
 
     XNAT.xhr.get({
-        url: XNAT.url.rootUrl('/xapi/dqr/adminProjectSettings/' + window.projectId),
+        url: XNAT.url.rootUrl('/xapi/dqr/isDqrProject/' + window.projectId),
         success: function(data){
-            if (data && data.enabled) {
+            if (/^true$/i.test(data)) {
                 pageBody.removeClass('hidden').show();
             }
             else {
-                showConfigErrorMessage();
+                showConfigErrorMessage('' +
+                    '<div class="error">' +
+                    'This project is not configured for PACS DICOM import. ' +
+                    'Please contact your system administrator if you need PACS DICOM import enabled.' +
+                    '</div>');
             }
         },
         failure: function(){
             console.warn(arguments);
-            showConfigErrorMessage();
+            showConfigErrorMessage('' +
+                '<div class="error">' +
+                'Access denied. Redirecting...' +
+                '</div>');
+            window.setTimeout(function(){
+                window.location.href = XNAT.url.rootUrl()
+            }, 2000)
         }
     });
 
