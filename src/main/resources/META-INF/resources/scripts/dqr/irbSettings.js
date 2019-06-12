3adf02a522247448520383aa034a883d55c35fd6
Mark M. Florida <markflorida@wustl.edu>
2019-06-12 15:01:00 -0500

    XNATX-785 Prevent double-encoding of IRB number; refactored IRB settings for improved performance.

--- src/main/resources/META-INF/resources/scripts/dqr/irbSettings.js
+++ src/main/resources/META-INF/resources/scripts/dqr/irbSettings.js
@@ -31,73 +31,74 @@
 
     var irbUrl = baseUrl + '/irbNumber';
 
-    // set the value of the input as soon as it's available
-    waitForElement(1, '#irb-number', function($el){
+    // set the value asap
+    XNAT.xhr.get({
+        url: XNAT.url.restUrl(irbUrl),
+        dataType: 'text',
+        success: function(irbNum){
 
-        var el = this;
+            // set the value of the input as soon as it's available
+            waitForElement(1, '#irb-number', function($el){
 
-        // set the value asap
-        XNAT.xhr.get({
-            url: XNAT.url.restUrl(irbUrl),
-            dataType: 'text',
-            success: function(irbNum){
+                var el = this;
+
                 el.value = irbNum;
-            },
-            failure: function(){
-                console.error(arguments);
-            }
-        });
 
-        // handle saving the number
-        $('#save-irb-number').on('click', function(e){
+                // handle saving the number
+                $('#save-irb-number').on('click', function(e){
 
-            e.preventDefault();
+                    e.preventDefault();
 
-            // var saveNumber = XNAT.xhr.put(XNAT.url.restUrl(irbUrl, { irbNumber: el.value }, false));
+                    var saveNumber = XNAT.xhr.put({
+                        url: XNAT.url.rootUrl(irbUrl + '?irbNumber=' + encodeURIComponent(el.value)),
+                        contentType: false,
+                        processData: false
+                    });
 
-            var saveNumber = XNAT.xhr.put({
-                method: 'PUT',
-                url: XNAT.url.restUrl(irbUrl),
-                data: {
-                    irbNumber: el.value
-                }
+                    saveNumber.done(function(){
+                        XNAT.ui.banner.top(2000, 'IRB Number Saved', 'success');
+                    });
+
+                    saveNumber.fail(function(){
+                        XNAT.ui.banner.top(2000, 'An error occurred. IRB number not saved.', 'error');
+                        console.error(arguments);
+                    });
+
+                });
+
             });
 
-            saveNumber.done(function(){
-                XNAT.ui.banner.top(2000, 'IRB Number Saved', 'success');
-            });
-
-            saveNumber.fail(function(){
-                console.error(arguments);
-            });
-
-        });
-
+        },
+        failure: function(){
+            console.error(arguments);
+        }
     });
 
-    // if there's a saved IRB file, show the link with file name
-    waitForElement(1, '#irb-file-link', function($el){
 
-        var link = this;
-        var div = document.getElementById('irb-file-download');
+    XNAT.xhr.get({
+        url: XNAT.url.restUrl(baseUrl + '/irbFilename'),
+        dataType: 'text',
+        success: function(filename){
 
-        // set the value asap
-        XNAT.xhr.get({
-            url: XNAT.url.restUrl(baseUrl + '/irbFilename'),
-            dataType: 'text',
-            success: function(filename){
-                if (filename) {
+            // if there's a saved IRB file, show the link with file name
+            if (filename) {
+
+                waitForElement(1, '#irb-file-link', function($el){
+
+                    var link = this;
+                    var div  = document.getElementById('irb-file-download');
+
                     link.textContent = filename;
-                    link.href = XNAT.url.rootUrl(baseUrl + '/irbFile/'+filename);
+                    link.href        = XNAT.url.rootUrl(baseUrl + '/irbFile/' + filename);
                     link.classList.add('link');
                     div.classList.remove('hidden');
-                }
-            },
-            failure: function(){
-                console.error(arguments);
-            }
-        });
+                });
 
+            }
+        },
+        failure: function(){
+            console.error(arguments);
+        }
     });
 
     return XNAT.plugin.dqr.irbSettings = irbSettings;
