31f5efc0f4116bce2f8e43ac944ddc7d6dd36636
Mike McKay <mfmckay@wustl.edu>
2020-01-02 12:13:38 -0600

    XNATX-838, XNATX-802 Made solution for inconsistent Study IDs only used when needed so it doesn't cause relabeling issues.

--- src/main/java/org/nrg/dqr/dicom/id/OverrideStudyIdExtractor.java
+++ src/main/java/org/nrg/dqr/dicom/id/OverrideStudyIdExtractor.java
@@ -8,6 +8,7 @@ import org.nrg.dqr.domain.entities.StudyIdStudyInstanceUidMapping;
 import org.nrg.dqr.preferences.DqrPreferences;
 import org.nrg.dqr.services.StudyIdStudyInstanceUidMappingService;
 import org.nrg.xdat.XDAT;
+import org.nrg.xnat.helpers.merge.anonymize.DefaultAnonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static org.nrg.framework.orm.DatabaseHelper.convertPGIntervalToIntSeconds;
@@ -26,25 +27,36 @@ public class OverrideStudyIdExtractor implements Extractor {
     public String extract(DicomObject o) {
         String studyId = o.getString(this.tags.get(0));
         String studyInstanceUID = o.getString(this.tags.get(1));
-
-        StudyIdStudyInstanceUidMappingService mappingService = XDAT.getContextService().getBean(StudyIdStudyInstanceUidMappingService.class);
-        List<StudyIdStudyInstanceUidMapping> mappings = mappingService.getAllForStudyInstanceUid(studyInstanceUID);
-        StudyIdStudyInstanceUidMapping mostRecentMapping = mappings.size()>0?mappings.get(0):null;
-        String interval = XDAT.getContextService().getBean(DqrPreferences.class).getAssumeSameSessionIfArrivedWithin();
-        Date assumeSameSessionIfArrivedAfter = new Date(System.currentTimeMillis() - (1000 * convertPGIntervalToIntSeconds(interval)));
-        if(mostRecentMapping!=null && mostRecentMapping.getCreated()!=null && mostRecentMapping.getCreated().after(assumeSameSessionIfArrivedAfter)){
-            //Mapping was added within the assumeSameSessionIfArrivedWithin interval, so this is likely a case of one study with multiple study IDs
-            if(!StringUtils.equals(studyId,mostRecentMapping.getStudyId())){
-                _log.error("DICOM files with the same StudyInstanceUID ("+studyInstanceUID+") and different study IDs ("+mostRecentMapping.getStudyId()+", "+studyId+") received. Since they were received within half an hour, XNAT is assuming the same session label should be used and is using the study ID that arrived first ("+mostRecentMapping.getStudyId()+") to determine that.");
-            }
-            return mostRecentMapping.getStudyId();
+        String script = null;
+        try {
+            script = DefaultAnonUtils.getService().getStudyScript(studyInstanceUID);
+        }
+        catch(Exception e){
+            _log.error("Error checking whether there was a relabel script for incoming data.",e);
+        }
+        if(script!=null && script.contains("(0020,0010)")){
+            //Study ID has already been relabeled, so even if Study IDs were inconsistent in the source data, they will have already been made consistent.
+            return studyId;
         }
         else{
-            StudyIdStudyInstanceUidMapping mapping = new StudyIdStudyInstanceUidMapping();
-            mapping.setStudyId(studyId);
-            mapping.setStudyInstanceUid(studyInstanceUID);
-            mappingService.create(mapping);
-            return studyId;
+            StudyIdStudyInstanceUidMappingService mappingService = XDAT.getContextService().getBean(StudyIdStudyInstanceUidMappingService.class);
+            List<StudyIdStudyInstanceUidMapping> mappings = mappingService.getAllForStudyInstanceUid(studyInstanceUID);
+            StudyIdStudyInstanceUidMapping mostRecentMapping = mappings.size() > 0 ? mappings.get(0) : null;
+            String interval = XDAT.getContextService().getBean(DqrPreferences.class).getAssumeSameSessionIfArrivedWithin();
+            Date assumeSameSessionIfArrivedAfter = new Date(System.currentTimeMillis() - (1000 * convertPGIntervalToIntSeconds(interval)));
+            if (mostRecentMapping != null && mostRecentMapping.getCreated() != null && mostRecentMapping.getCreated().after(assumeSameSessionIfArrivedAfter)) {
+                //Mapping was added within the assumeSameSessionIfArrivedWithin interval, so this is likely a case of one study with multiple study IDs
+                if (!StringUtils.equals(studyId, mostRecentMapping.getStudyId())) {
+                    _log.error("DICOM files with the same StudyInstanceUID (" + studyInstanceUID + ") and different study IDs (" + mostRecentMapping.getStudyId() + ", " + studyId + ") received. Since they were received within half an hour, XNAT is assuming the same session label should be used and is using the study ID that arrived first (" + mostRecentMapping.getStudyId() + ") to determine that.");
+                }
+                return mostRecentMapping.getStudyId();
+            } else {
+                StudyIdStudyInstanceUidMapping mapping = new StudyIdStudyInstanceUidMapping();
+                mapping.setStudyId(studyId);
+                mapping.setStudyInstanceUid(studyInstanceUID);
+                mappingService.create(mapping);
+                return studyId;
+            }
         }
     }
 
